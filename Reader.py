d916e30595957a62d3f45888d47ef283e286804a
Youssef Bonnaire <yb2010@hw.ac.uk>
2021-12-01 18:52:33 +0000

    Initialised Reader.py file

---
+++ Reader.py
@@ -0,0 +1,10 @@
+# Reader profiles: In order to develop a readership profile for the site, we want to identify the most
+# avid readers. We want to determine, for each user, the total time spent reading documents. The top 10
+# readers, based on this analysis, should be printed.
+import pandas as pd
+
+dataset = pd.read_json('issuu_cw2.json', lines=True)
+
+def top10():
+
+    return None
