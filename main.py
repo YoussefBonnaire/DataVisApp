c59f06e37c15d88fdb8d9ebf2207216bbb3cb365
Youssef Bonnaire <yb2010@hw.ac.uk>
2021-12-05 16:32:15 +0000

    user change

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
-import Reader
 import Viewer
 import alsoLikes
+
 
 def main():
     """ Testing different methods """
     document = '140228202800-6ef39a241f35301a9a42cd0ed21e5fb0'
-    user = 'dd326898d5605e63'
+    user = 'b2a24f14bb5c9ea3'
     country_group, df = Viewer.Get_countries(document)
     print(country_group)
     continents = Viewer.Get_continents(df)
     print(continents)
     browsers = Viewer.Get_browser(document)
     print(browsers)
+
     documents = alsoLikes.userHasRead(user)
     print('The user has read')
     print(documents)
     print('Document 1 has been read by')
-    users=alsoLikes.findReaders(documents[0])
+    users = alsoLikes.findReaders(documents[0])
     print(users)
-    #top10 = Reader.top10()
-    #print(top10)
+    # top10 = Reader.top10()
+    # print(top10)
     return 0
 
 
