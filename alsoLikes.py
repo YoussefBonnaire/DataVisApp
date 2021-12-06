a13c887ce14c3e8a44930238d1a9a93049b7b422
jm232 <jm232@hw.ac.uk>
2021-12-06 11:42:19 +0000

    Fixed the issue in graph generation

--- alsoLikes.py
+++ alsoLikes.py
@@ -1,10 +1,9 @@
 from collections import Counter
 
-import graphviz
 import numpy
 
 import Viewer as v
-
+import graphviz
 
 #  Returns the a list of visitor ids for the users
 #  that have visited the document
@@ -35,41 +34,39 @@ def sortDocuments(documents):
 #  and returns a list of the ten documents that have been
 #  read by the most users, sorted with the inputted sorting algorithm
 #  that have also read the input document
-def alsoLikes(document, user=None, sortF=None):
+def alsoLikes(document,user=None,sortF=None):
     users = findReaders(document)
-    if user is not None:
-        numpy.append(users, user)
+    if(user!=None):
+        numpy.append(users,user)
     documents = []
     for user in users:
         userDocs = userHasRead(user)
         documents.extend(userDocs)
-    if sortF is None:
+    if(sortF==None):
         documents = sortDocuments(documents)[:9]
     else:
-        document = sortF(documents)[:9]
+        document= sortF(documents)[:9]
     return documents
-
-
 #
-def buildGraph(documentIn, userIn=None, sortF=None):
-    documents = alsoLikes(documentIn, userIn, sortF)
-    graph = graphviz.Graph
-    addedUsers = []
+def buildGraph(documentIn,userIn=None,sortF=None):
+    documents = alsoLikes(documentIn,userIn,sortF)
+    graph = graphviz.Graph()
+    addedUsers =[]
     graph.node('Readers', 'Readers', shape='plaintext', rank='Readers')
     graph.node('Documents', 'Documents', shape='plaintext', rank='Documents')
     graph.edge('Readers', 'Documents')
     for document in documents:
         docNodeLabel = document[41:45]
-        if document == documentIn:
-            graph.node(document, docNodeLabel, color='green', shape='circle', rank='Readers')
+        if(document==documentIn):
+            graph.node(document, docNodeLabel, style='filled', color='green', shape='circle', rank='Readers')
         else:
             graph.node(document, docNodeLabel, shape='circle', rank='Readers')
-        users = findReaders(document)
+        users=findReaders(document)
         for user in users:
             userNodeLabel = user[11:15]
             if not addedUsers.__contains__(user):
-                if user == userIn:
-                    graph.node(user, docNodeLabel, color='green', shape='box', rank='Readers')
+                if(user==userIn):
+                    graph.node(user, docNodeLabel, style='filled', color='green', shape='box', rank='Readers')
                 else:
                     graph.node(user, docNodeLabel, shape='box', rank='Readers')
             graph.edge(user, document)
